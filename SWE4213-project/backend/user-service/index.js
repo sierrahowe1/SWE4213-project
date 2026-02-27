77d1f112f9cf5fa59bb9140ecbcba77f6e5aff3e
sierrahowe1 <sierra.howe@unb.ca>
2026-02-26 23:13:18 -0400

    adding filler endpoints for structure

--- SWE4213-project/backend/user-service/index.js
+++ SWE4213-project/backend/user-service/index.js
@@ -1,10 +1,37 @@
 const express = require('express');
+const amqp = require('amqplib');
 const app = express();
 
-const PORT = process.env.PORT || 5000;
+const PORT = process.env.PORT || 3001;
 app.use(express.json());
 
 
-app.get('/users', (req, res) => {
-    
-})
+app.post('/auth/login', (req, res) => {
+    const { username, password } = req.body;
+    res.json({ message: 'Filler login success message'});
+});
+
+app.post('/auth/register', (req, res) => {
+    const { username, password } = req.body;
+    res.json({ message: 'Filler register success message'});
+
+});
+
+app.get('auth/status', (req, res) => {
+    res.json({ message: 'Filler auth status message' });
+});
+
+app.get('/users/:id', (req, res) => {
+    const userId = req.params.id;
+    res.json({ id: userId, username: 'fillerUser'});
+});
+
+app.get('/userBooks/:userId', (req, res) => {
+    res.json({ userId: req.params.userId, books: ['Book1', 'Book2']});
+});
+
+app.put('/userBooks/:userId', (req, res) => {
+    const userId = req.params.userId;
+    const { books } = req.body;
+    res.json({ message: `Updated books for user ${userId}`, books });
+});
