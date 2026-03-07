f6c67dc5402b60674f78cd657e9233350735494e
madisonb2 <mbrown31@unb.ca>
2026-03-06 21:45:15 -0400

    updated prisma schema

--- SWE4213-project/book-service/init.sql
+++ SWE4213-project/book-service/init.sql
@@ -8,7 +8,7 @@ CREATE TABLE IF NOT EXISTS books (
     cover_url TEXT
 );
 
-INSERT INTO books (title, author, genre, summary, cover_url) VALUES
-    ('The Hunger Games', 'Suzanne Collins', 'Science fiction', 'The Hunger Games follows 16-year-old Katniss Everdeen, a girl from District 12 who volunteers for the 74th Hunger Games in place of her younger sister Primrose Everdeen.', 'https://upload.wikimedia.org/wikipedia/en/d/dc/The_Hunger_Games.jpg'),
-    ('Romeo and Juliet', 'William Shakespeare', 'Tragedy', 'Tragedy written by William Shakespeare about the romance between two young Italians from feuding families', 'https://m.media-amazon.com/images/S/compressed.photo.goodreads.com/books/1629680008i/18135.jpg');
+INSERT INTO books (title, author, total_pages, genre, summary, cover_url) VALUES
+    ('The Hunger Games', 'Suzanne Collins', 'Science fiction', 374, 'The Hunger Games follows 16-year-old Katniss Everdeen, a girl from District 12 who volunteers for the 74th Hunger Games in place of her younger sister Primrose Everdeen.', 'https://upload.wikimedia.org/wikipedia/en/d/dc/The_Hunger_Games.jpg'),
+    ('Romeo and Juliet', 'William Shakespeare', 'Tragedy', 96, 'Tragedy written by William Shakespeare about the romance between two young Italians from feuding families', 'https://m.media-amazon.com/images/S/compressed.photo.goodreads.com/books/1629680008i/18135.jpg');
 
